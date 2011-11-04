aa0a63baea043168385fd6e108391452d6a050fe
Edward Waller <edward@wallerdev.com>
2011-11-04 19:37:21 -0400

    Renamed library to WallerDev.Decaptcher.

--- Decaptcher.Test/DecaptcherTest.cs
+++ Decaptcher.Test/DecaptcherTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using WallerDev.Decaptcher;
 
 namespace Decaptcher.Test
 {
--- Decaptcher/APIConstants.cs
+++ Decaptcher/APIConstants.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 
-namespace Decaptcher
+namespace WallerDev.Decaptcher
 {
     public enum PictureType
     {
